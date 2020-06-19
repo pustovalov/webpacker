5631ae4155845db90656157c59e0d901c099ff82
Pavel Pustovalov <pustovalov.pavel@gmail.com>
2020-06-19 17:44:46 +0300

    debug

--- test/test_app/config/application.rb
+++ test/test_app/config/application.rb
@@ -1,9 +1,8 @@
-require "action_controller/railtie"
-require "action_view/railtie"
+require "rails/all"
 require "webpacker"
 
 module TestApp
-  class Application < ::Rails::Application
+  class Application < Rails::Application
     config.secret_key_base = "abcdef"
     config.eager_load = true
     config.active_support.test_order = :sorted
